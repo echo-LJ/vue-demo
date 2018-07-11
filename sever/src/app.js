b00834f1f5efb54eaa3e37fa2ef3cb11ec7bd133
lujing15@jd.com <lujing15@jd.com;lujing_girl@163.com>
2018-07-11 17:43:40 +0800

    sever

--- sever/src/app.js
+++ sever/src/app.js
@@ -2,16 +2,22 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
+const {sequelize} = require('./models')
+const config = require('./config/config')
 
 const app = express()
 app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
-app.post('/register', (req, res) => {
-  res.send({
-    message: `${req.body.email}! hello fun!`
+require('./passport')
+
+require('./routes')(app)
+
+sequelize.sync({force: false})
+  .then(() => {
+    app.listen(config.port)
+    console.log(`Server started on port ${config.port}`)
   })
-})
 // 环境变量设置了prot就用环境变量的port,否则就用8081
-app.listen(process.env.PORT || 8081)
+// app.listen(process.env.PORT || 8081)
